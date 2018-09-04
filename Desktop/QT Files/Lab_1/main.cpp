dff34cd5810786ef79f23a843925575f8edb3d84
Xavier Carrillo <xcarrillo@smu.edu>
2018-09-04 11:47:49 -0500

    Lab1-Update2

---
+++ Desktop/QT Files/Lab_1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+<<<<<<< HEAD
+#include "merge.h"
+#include "bubble.h"
+#include "insertion.h"
+#include "createfile.h"
+#include "sort.h"
+=======
+>>>>>>> 7c751f06a766bdcc56bfeb56c389c3c3e574e8e2
+using namespace std;
+
+int main()
+{
+    cout << "You can do it Xavier, I believe in you" << endl;
+<<<<<<< HEAD
+    /*vector<int> nums= {4,9,2,5,8,1,7,78,21,45,33,112,92,31};
+    insertionsort(nums,nums.size());
+    for(int i=0;i<nums.size();i++)
+        cout << nums[i] << endl;
+    return 0;*/
+    runSort();
+
+=======
+    return 0;
+>>>>>>> 7c751f06a766bdcc56bfeb56c389c3c3e574e8e2
+}
